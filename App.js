ff03d5d3e74636633dda5a2d680be0567fb380df
adamalfirdauz <adam_22@apps.ipb.ac.id>
2018-03-19 11:12:06 +0700

    first init

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Codepanda x Yourganic present.</Text>
+        <Text>iOS test.</Text>
+        <Text>React Native memang mantap.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
